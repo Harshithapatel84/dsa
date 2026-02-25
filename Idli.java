5fd91928828daf0f98ee956fcf26375738ef7f52
kruthika <harshithashanthraju@gmail.com>
2026-02-25 22:56:45 +0530

    dsa

---
+++ Idli.java
@@ -0,0 +1,8 @@
+class Idli{
+  public static void main(String[] args)
+  {
+	  String name="idli";
+	  String rev=  new StringBuilder(name).reverse().toString();
+	  System.out.println(rev);
+  }
+  }

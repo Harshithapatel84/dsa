38c0d2077445d89c6a2bc34e397e825f9084ee10
kruthika <harshithashanthraju@gmail.com>
2026-03-05 00:27:35 +0530

    array

---
+++ ArrayWord.java
@@ -0,0 +1,14 @@
+class ArrayWord{
+	public static void main(String[] args){
+		String names[]={"riya","rakesh","pragathi","simran","rahul"};
+		for(int i=0;i<names.length;i++)
+		{
+			if(names[i].length() > 4)
+		{
+			System.out.println(names[i]);
+		}
+	}
+	}
+}
+
+

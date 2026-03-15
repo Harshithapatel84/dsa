7d087539ae9d0f64c9fbf9c10313efbd4e44668a
kruthika <harshithashanthraju@gmail.com>
2026-03-15 22:23:09 +0530

    factorial

---
+++ Factorial.java
@@ -0,0 +1,12 @@
+class Factorial{
+	public static void main(String[] args)
+	{
+		int num=2;
+		int fact=1;
+		for(int i=1;i<=num;i++)
+		fact=fact*i;
+		{
+			System.out.println("factorial is:"+fact);
+		}
+	}
+}

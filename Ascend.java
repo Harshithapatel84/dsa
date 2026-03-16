66959122c61ef5f8882e6ac4700f311463ff0c4e
kruthika <harshithashanthraju@gmail.com>
2026-03-16 20:32:18 +0530

    ascend

---
+++ Ascend.java
@@ -0,0 +1,22 @@
+class Ascend{
+	public static void main(String... values)
+	{
+		int num[]={5,2,8,1,4};
+		for(int i=0;i<num.length;i++)
+		{
+			for(int j=i+1;j<num.length;j++)
+			{
+	if(num[i]>num[j])
+	{
+		int temp=num[i];
+		num[i]=num[j];
+		num[j]=temp;
+	}
+	}
+	}
+	for(int i=0;i<num.length;i++)
+	{
+		System.out.println(num[i]);
+	}
+	}
+}

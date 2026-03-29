3969fd7304c70c53f1de830b4002e60f9b5cff49
kruthika <harshithashanthraju@gmail.com>
2026-03-29 22:02:07 +0530

    bitshift

---
+++ BitShift.java
@@ -0,0 +1,13 @@
+class BitShift 
+{
+    public static void main(String[] args) 
+	{
+		int num=5;
+        int leftShift=num<<1;
+		int rightShift=num>>1;
+
+        System.out.println("Original Number:"+num);
+        System.out.println("Left Shift:"+leftShift);
+        System.out.println("Right Shift:"+rightShift);
+    }
+}

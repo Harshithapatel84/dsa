4b2713c299c6820436b71da10713daa3c57cd190
kruthika <harshithashanthraju@gmail.com>
2026-03-31 22:59:51 +0530

    multidimensional

---
+++ MultiDimensionalarray.java
@@ -0,0 +1,18 @@
+class MultiDimensionalarray{
+	public static void main(String... values)
+	{
+		int[][] numbers={
+			{1,2,3},
+		    {4,5,6},
+			{7,8,9}
+			};
+		for(int i=0;i<numbers.length;i++)
+		{
+			for(int j=0;j<numbers.length;j++)
+			{
+				System.out.print(numbers[i][j]+ " ");
+			}
+			 System.out.println(); 
+		}
+	}
+}
